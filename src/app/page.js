20eef4300f24cae7319d2df42b0fb51f1515a3b9
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2024-08-02 22:30:59 -0300

    2'

--- src/app/page.js
+++ src/app/page.js
@@ -821,9 +821,45 @@ export default function Home() {
         />
       </div>
       <div className="flex flex-col items-center">
-        <p className="text-white font-bold text-3xl text-center tracking-tighter mt-5 md:text-7xl ">DESCUENTO ESPECIAL<br/> POR TIEMPO LIMITADO</p>
+        <p className="text-white font-bold text-3xl text-center tracking-tighter mt-5 md:text-7xl ">
+          DESCUENTO ESPECIAL
+          <br /> POR TIEMPO LIMITADO
+        </p>
         <img src="/flechas.webp" />
-        <Button className='mt-10' size='lg'><SendHorizonal className="mr-2 "/>Comienza YA</Button>
+        <Button className="mt-10" size="lg">
+          <SendHorizonal className="mr-2 " />
+          Comienza YA
+        </Button>
+        <p className="text-white tracking-tighter  mx-2 mt-5">
+          Este curso de Hidroponía es el puente de contacto con un experto en el
+          tema, que te llevará de la mano en todo el camino,
+          <br /> allanando los obstáculos con su conocimiento y vasta
+          experiencia lograda a lo largos de muchos años.
+        </p>
+        <Button
+          size="lg"
+          className='mt-5 bg-black hover:bg-slate-800'
+          onClick={() => {
+            push("https://wa.link/6nvgae");
+            trackEvent("InitiateCheckout", { value: 29.99, currency: "USD" });
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={20}
+            className="mr-2"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="#f5f5f7"
+              fillRule="evenodd"
+              d="M3.5 12a8.5 8.5 0 118.5 8.5 8.455 8.455 0 01-4.542-1.314 1 1 0 00-.821-.113l-2.88.859 1.085-2.537a1 1 0 00-.07-.92A8.456 8.456 0 013.5 12zM12 1.5C6.201 1.5 1.5 6.201 1.5 12c0 1.838.473 3.568 1.305 5.073L1.08 21.107a1 1 0 001.206 1.351l4.5-1.342A10.456 10.456 0 0012 22.5c5.799 0 10.5-4.701 10.5-10.5S17.799 1.5 12 1.5zm2.293 12.682l-1.315.926A9.338 9.338 0 0111 13.585a10.202 10.202 0 01-1.624-2.114l.835-.709a1 1 0 00.236-1.232l-1.064-2a1 1 0 00-1.54-.283l-.316.275c-.759.662-1.208 1.75-.836 2.852A12.049 12.049 0 009.586 15c1.813 1.813 3.655 2.527 4.733 2.805.868.223 1.689-.076 2.268-.548l.591-.482a1 1 0 00-.12-1.634l-1.678-1a1 1 0 00-1.088.041z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+          Contactanos
+        </Button>
       </div>
       <div className="container mx-auto bg-green-200  px-4 py-8 mt-10 pb-28 rounded-b-[20px] rounded-t-[20px]">
         <section className="mb-8">
